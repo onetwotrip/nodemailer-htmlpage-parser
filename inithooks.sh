1470c08a30411b9a0d8025c887480d276c331ad5
OneTwoTrip <services@onetwotrip.com>
2014-09-09 11:37:59 +0000

    inithooks.sh created online with Bitbucket

---
+++ inithooks.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Add commit-msg hook which prepends branchname to all commit messages
+#
+echo "adding commit-msg hook"
+cat <<- "EOF" > .git/hooks/commit-msg
+	#!/bin/sh
+	#
+	# Automatically add branch name and branch description to every commit message except merge commit.
+	#
+
+	COMMIT_EDITMSG=$1
+
+	addBranchName() {
+	  NAME=$(git branch | grep '*' | sed 's/* //')
+	  DESCRIPTION=$(git config branch."$NAME".description)
+	  echo "$NAME: $(cat $COMMIT_EDITMSG)" > $COMMIT_EDITMSG
+	  if [ -n "$DESCRIPTION" ]
+	  then
+		 echo "" >> $COMMIT_EDITMSG
+		 echo $DESCRIPTION >> $COMMIT_EDITMSG
+	  fi
+	}
+
+	MERGE=$(cat $COMMIT_EDITMSG|grep -i 'merge'|wc -l)
+
+	if [ $MERGE -eq 0 ] ; then
+	  addBranchName
+	fi
+EOF
+
+chmod +x .git/hooks/commit-msg
